e19858f9d1a8da744efec13c871b423f3a4a2e9c
Crithian Campelo <campelo.cfc@gmail.com>
2025-11-25 22:43:23 -0300

    Atualiza banco de dados com novas plantas e padronização de imagens

--- api/database/popular-banco.js
+++ api/database/popular-banco.js
@@ -5,257 +5,312 @@ const DB_PATH = path.join(__dirname, 'plantas.db');
 const db = new sqlite3.Database(DB_PATH);
 
 const todasAsPlantas = [
+    // Espada São Jorge
     {
-        nome: 'Palmeira Ráfia',
-        valor: 50.00,
-        imagem: 'images/Plantas/CatalogoDePlantas/Medias/Planta-palmeira-rafia-media.jpg',
-        tamanho: 'Média'
-    },
-    {
-        nome: 'Croton',
-        valor: 40.00,
-        imagem: 'images/Plantas/CatalogoDePlantas/Pequenas/Planta-croton-pequena.jpg',
-        tamanho: 'Pequena'
-    },
-    {
-        nome: 'Costela de Adão',
-        valor: 50.00,
-        imagem: 'images/Plantas/CatalogoDePlantas/Medias/Planta-costela-de-adao-medio.jpg',
-        tamanho: 'Média'
-    },
-    {
-        nome: 'Aglaonema Red Valentine',
-        valor: 40.00,
-        imagem: 'images/Plantas/CatalogoDePlantas/Pequenas/Planta-aglaonema-red-valentine-pequena.jpg',
-        tamanho: 'Pequena'
-    },
-    {
-        nome: 'Dinheiro em Penca Pilear',
+        nome: 'Espada São Jorge',
         valor: 15.00,
-        imagem: 'images/Plantas/CatalogoDePlantas/Medias/Planta-dinheiro-em-penca-media.jpg',
-        tamanho: 'Média'
+        imagem: 'images/Plantas/CatalogoDePlantas/Pequenas/p-espada-sao-jorge.jpeg',
+        tamanho: 'Pequena'
     },
+    // Espadadinha Santa Bárbara
     {
-        nome: 'Cróton Misto Brasileirinho',
-        valor: 25.00,
-        imagem: 'images/Plantas/CatalogoDePlantas/Grandes/Planta-croton-brasileirinho-grande.jpg',
-        tamanho: 'Grande'
-    },
-    {
-        nome: 'Lírio do Amazonas',
+        nome: 'Espadadinha Santa Bárbara',
         valor: 20.00,
-        imagem: 'images/Plantas/CatalogoDePlantas/Pequenas/Planta-lirio-amazonas-pequena.jpg',
+        imagem: 'images/Plantas/CatalogoDePlantas/Pequenas/p-espada-sao-jorge-clara.jpeg',
         tamanho: 'Pequena'
     },
+    // Espada Santa Bárbara
     {
-        nome: 'Lírio do Amazonas',
-        valor: 30.00,
-        imagem: 'images/Plantas/CatalogoDePlantas/Grandes/lirio-amazonas-grande.jpg',
-        tamanho: 'Grande'
-    },
-    {
-        nome: 'Lírio da Paz',
-        valor: 10.00,
-        imagem: 'images/Plantas/CatalogoDePlantas/Pequenas/Planta-lirio-da-paz-pequena.jpg',
-        tamanho: 'Pequena'
-    },
-    {
-        nome: 'Lírio da Paz',
+        nome: 'Espada Santa Bárbara',
         valor: 15.00,
-        imagem: 'images/Plantas/CatalogoDePlantas/Medias/lirio-paz-media.jpg', //Falta imagem
-        tamanho: 'Média'
-    },
-    {
-        nome: 'Lírio da Paz',
-        valor: 30.00,
-        imagem: 'images/Plantas/CatalogoDePlantas/Grandes/lirio-paz-grande.jpg', // Falta imagem
-        tamanho: 'Grande'
-    },
-    {
-        nome: 'Palmeira Ráfia com Mistos',
-        valor: 40.00,
-        imagem: 'images/Plantas/CatalogoDePlantas/Medias/Planta-palmeira-rafia-misto-media.jpg',
-        tamanho: 'Média'
-    },
-    {
-        nome: 'Maranta Cinza',
-        valor: 20.00,
-        imagem: 'images/Plantas/CatalogoDePlantas/Pequenas/Planta-maranta-cinza-pequena.jpg',
+        imagem: 'images/Plantas/CatalogoDePlantas/Pequenas/p-espada-santa-barbara.jpeg',
         tamanho: 'Pequena'
     },
+    // Comigo-ninguém-pode
     {
-        nome: 'Zamiocuca',
+        nome: 'Comigo-ninguém-pode',
         valor: 15.00,
-        imagem: 'images/Plantas/CatalogoDePlantas/Pequenas/zamiocuca-pequena.jpg', //Falta Imagem
-        tamanho: 'Pequena'
-    },
-    {
-        nome: 'Zamiocuca',
-        valor: 25.00,
-        imagem: 'images/Plantas/CatalogoDePlantas/Medias/zamiocuca-media.jpg', //Falta Imagem
-        tamanho: 'Média'
-    },
-    {
-        nome: 'Zamiocuca',
-        valor: 50.00,
-        imagem: 'images/Plantas/CatalogoDePlantas/Grandes/Planta-zamiocuca-grande.jpg',
-        tamanho: 'Grande'
-    },
-    {
-        nome: 'Aglaonema Café de Salão - Comigo ninguém pode',
-        valor: 20.00,
-        imagem: 'images/Plantas/CatalogoDePlantas/Pequenas/Planta-comigo-ninguem-pode-pequena.jpg',
-        tamanho: 'Pequena'
-    },
-    {
-        nome: 'Aglaonema Café de Salão - Comigo ninguém pode',
-        valor: 50.00,
-        imagem: 'images/Plantas/CatalogoDePlantas/Grandes/aglaonema-cafe-grande.jpg', //Falta Imagem
-        tamanho: 'Grande'
-    },
-    {
-        nome: 'Areca Bambu',
-        valor: 50.00,
-        imagem: 'images/Plantas/CatalogoDePlantas/Medias/Planta-areca-bambu-media.jpg',
-        tamanho: 'Média'
-    },
-    {
-        nome: 'Grama Pelo de Urso',
-        valor: 30.00,
-        imagem: 'images/Plantas/CatalogoDePlantas/Medias/Planta-grama-pelo-de-urso-media.jpg',
-        tamanho: 'Média'
-    },
-    {
-        nome: 'Espada de São Jorge',
-        valor: 10.00,
-        imagem: 'images/Plantas/CatalogoDePlantas/Pequenas/espada-sao-jorge-pequena.jpg', //Falta Imagem
+        imagem: 'images/Plantas/CatalogoDePlantas/Pequenas/p-comigo-ninguem-pode.jpeg',
         tamanho: 'Pequena'
     },
     {
         nome: 'Comigo-ninguém-pode',
-        valor: 25.00,
-        imagem: 'images/Plantas/CatalogoDePlantas/Medias/comigo-ninguem-pode-media.jpg', //Falta Imagem
+        valor: 15.00,
+        imagem: 'images/Plantas/CatalogoDePlantas/Medias/m-comigo-ninguem-pode.jpeg',
         tamanho: 'Média'
     },
     {
         nome: 'Comigo-ninguém-pode',
-        valor: 25.00,
-        imagem: 'images/Plantas/CatalogoDePlantas/Grandes/comigo-ninguem-pode-grande.jpg', //Falta Imagem
+        valor: 15.00,
+        imagem: 'images/Plantas/CatalogoDePlantas/Grandes/g-comigo-ninguem-pode.jpeg',
         tamanho: 'Grande'
     },
+    // Dois Amores
     {
         nome: 'Dois Amores',
         valor: 15.00,
-        imagem: 'images/Plantas/CatalogoDePlantas/Pequenas/dois-amores-pequena.jpg', //Falta Imagem
+        imagem: 'images/Plantas/CatalogoDePlantas/Pequenas/p-dois-amores.jpeg',
         tamanho: 'Pequena'
     },
+    // Bambu da Sorte
     {
         nome: 'Bambu da Sorte',
+        valor: 20.00,
+        imagem: 'images/Plantas/CatalogoDePlantas/Medias/m-bamboo-da-sorte.jpeg',
+        tamanho: 'Média'
+    },
+    // Antúrio / Alocácia
+    {
+        nome: 'Antúrio / Alocácia',
+        valor: 10.00,
+        imagem: 'images/Plantas/CatalogoDePlantas/Pequenas/p-anturio.jpeg',
+        tamanho: 'Pequena'
+    },
+    {
+        nome: 'Antúrio / Alocácia',
+        valor: 15.00,
+        imagem: 'images/Plantas/CatalogoDePlantas/Medias/m-anturio.jpeg',
+        tamanho: 'Média'
+    },
+    {
+        nome: 'Antúrio / Alocácia',
         valor: 25.00,
-        imagem: 'images/Plantas/CatalogoDePlantas/Medias/bambu-sorte-media.jpg', //Falta Imagem
-        tamanho: 'Média'
-    },
-    {
-        nome: 'Antúrio',
-        valor: 10.00,
-        imagem: 'images/Plantas/CatalogoDePlantas/Pequenas/anturio-pequena.jpg', //Falta Imagem
-        tamanho: 'Pequena'
-    },
-    {
-        nome: 'Antúrio',
-        valor: 15.00,
-        imagem: 'images/Plantas/CatalogoDePlantas/Medias/anturio-media.jpg', //Falta Imagem
-        tamanho: 'Média'
-    },
-    {
-        nome: 'Antúrio',
-        valor: 20.00,
-        imagem: 'images/Plantas/CatalogoDePlantas/Grandes/anturio-grande.jpg', //Falta Imagem
+        imagem: 'images/Plantas/CatalogoDePlantas/Grandes/g-anturio.jpeg',
         tamanho: 'Grande'
     },
+    // Ora-pro-nóbis
     {
-        nome: 'Ora pro nobis',
+        nome: 'Ora-pro-nóbis',
         valor: 10.00,
-        imagem: 'images/Plantas/CatalogoDePlantas/Pequenas/ora-pro-nobis-pequena.jpg', //Falta Imagem
+        imagem: 'images/Plantas/CatalogoDePlantas/Pequenas/p-ora-pro-nobis (2).jpeg',
+        tamanho: 'Pequena'
+    },
+    // Aglaonema Vermelho
+    {
+        nome: 'Aglaonema Vermelho',
+        valor: 20.00,
+        imagem: 'images/Plantas/CatalogoDePlantas/Pequenas/p-aglaonema-vermelho.jpeg',
         tamanho: 'Pequena'
     },
     {
-        nome: 'Ora pro nobis',
-        valor: 15.00,
-        imagem: 'images/Plantas/CatalogoDePlantas/Medias/ora-pro-nobis-media.jpg', //Falta Imagem
+        nome: 'Aglaonema Vermelho',
+        valor: 50.00,
+        imagem: 'images/Plantas/CatalogoDePlantas/Medias/m-aglaonema-vermelho.jpg',
         tamanho: 'Média'
     },
+    // Avenca
     {
-        nome: 'Aglaonema Vermelha',
+        nome: 'Avenca',
         valor: 20.00,
-        imagem: 'images/Plantas/CatalogoDePlantas/Pequenas/aglaonema-vermelha-pequena.jpg', //Falta Imagem
+        imagem: 'images/Plantas/CatalogoDePlantas/Medias/m-avenca.jpeg', // Placeholder: Imagem Pequena não encontrada
         tamanho: 'Pequena'
-    },
-    {
-        nome: 'Aglaonema Vermelha',
-        valor: 40.00,
-        imagem: 'images/Plantas/CatalogoDePlantas/Grandes/aglaonema-vermelha-grande.jpg', //Falta Imagem
-        tamanho: 'Grande'
     },
     {
         nome: 'Avenca',
         valor: 40.00,
-        imagem: 'images/Plantas/CatalogoDePlantas/Medias/avenca-media.jpg', //Falta Imagem
+        imagem: 'images/Plantas/CatalogoDePlantas/Medias/m-avenca.jpeg',
         tamanho: 'Média'
     },
-    {
-        nome: 'Avenca',
-        valor: 60.00,
-        imagem: 'images/Plantas/CatalogoDePlantas/Grandes/avenca-grande.jpg', //Falta Imagem
-        tamanho: 'Grande'
-    },
+    // Árvore da Felicidade
     {
         nome: 'Árvore da Felicidade',
         valor: 20.00,
-        imagem: 'images/Plantas/CatalogoDePlantas/Pequenas/arvore-felicidade-pequena.jpg', //Falta Imagem
+        imagem: 'images/Plantas/CatalogoDePlantas/Pequenas/p-arvore-da-felicidade-femea.jpeg',
         tamanho: 'Pequena'
     },
     {
         nome: 'Árvore da Felicidade',
-        valor: 30.00,
-        imagem: 'images/Plantas/CatalogoDePlantas/Medias/arvore-felicidade-media.jpg', //Falta Imagem
+        valor: 25.00,
+        imagem: 'images/Plantas/CatalogoDePlantas/Medias/m-arvore-da-felicidade.jpeg',
         tamanho: 'Média'
     },
+    // Dracena Vermelha
     {
         nome: 'Dracena Vermelha',
         valor: 10.00,
-        imagem: 'images/Plantas/CatalogoDePlantas/Pequenas/dracena-vermelha-pequena.jpg', //Falta Imagem
+        imagem: 'images/Plantas/CatalogoDePlantas/Pequenas/p-dracena-vermelha.jpeg',
         tamanho: 'Pequena'
     },
     {
         nome: 'Dracena Vermelha',
         valor: 20.00,
-        imagem: 'images/Plantas/CatalogoDePlantas/Medias/dracena-vermelha-media.jpg', //Falta Imagem
+        imagem: 'images/Plantas/CatalogoDePlantas/Medias/m-dracena-vermelha.jpeg',
         tamanho: 'Média'
     },
+    // Orquídea Iris
     {
-        nome: 'Orquídea Íris',
-        valor: 20.00,
-        imagem: 'images/Plantas/CatalogoDePlantas/Medias/orquidea-iris-media.jpg', //Falta Imagem
-        tamanho: 'Média'
-    },
-    {
-        nome: 'Aglaonema Comigo-ninguém-pode',
+        nome: 'Orquídea Iris',
         valor: 25.00,
-        imagem: 'images/Plantas/CatalogoDePlantas/Pequenas/aglaonema-comigo-ninguem-pode-pequena.jpg', //Falta Imagem
+        imagem: 'images/Plantas/CatalogoDePlantas/Medias/m-orquidia-iris.jpeg',
+        tamanho: 'Média'
+    },
+    // Jiboia
+    {
+        nome: 'Jiboia',
+        valor: 10.00,
+        imagem: 'images/Plantas/CatalogoDePlantas/Grandes/g-jiboia.jpeg', // Placeholder: Imagem Pequena não encontrada
         tamanho: 'Pequena'
     },
     {
         nome: 'Jiboia',
+        valor: 20.00,
+        imagem: 'images/Plantas/CatalogoDePlantas/Grandes/g-jiboia.jpeg',
+        tamanho: 'Grande'
+    },
+    // Aspargo Alfinete
+    {
+        nome: 'Aspargo Alfinete',
+        valor: 20.00,
+        imagem: 'images/Plantas/CatalogoDePlantas/Medias/m-aspargo-alfinete.jpeg',
+        tamanho: 'Média'
+    },
+    // Palmeira Ráfia
+    {
+        nome: 'Palmeira Ráfia',
         valor: 30.00,
-        imagem: 'images/Plantas/CatalogoDePlantas/Medias/jiboia-media.jpg', //Falta Imagem
+        imagem: 'images/Plantas/CatalogoDePlantas/Grandes/g-palmeira-rafia.jpg', // Placeholder: Imagem Média não encontrada
         tamanho: 'Média'
     },
     {
-        nome: 'Aspargo Alfinete',
+        nome: 'Palmeira Ráfia',
+        valor: 50.00,
+        imagem: 'images/Plantas/CatalogoDePlantas/Grandes/g-palmeira-rafia.jpg',
+        tamanho: 'Grande'
+    },
+    // Cróton
+    {
+        nome: 'Cróton',
         valor: 40.00,
-        imagem: 'images/Plantas/CatalogoDePlantas/Medias/aspargo-alfinete-media.jpg', //Falta Imagem
+        imagem: 'images/Plantas/CatalogoDePlantas/Medias/m-croton.jpg',
         tamanho: 'Média'
+    },
+    // Costela de Adão
+    {
+        nome: 'Costela de Adão',
+        valor: 80.00,
+        imagem: 'images/Plantas/CatalogoDePlantas/Grandes/g-costela-de-adao.jpg',
+        tamanho: 'Grande'
+    },
+    // Dinheiro-em-penca
+    {
+        nome: 'Dinheiro-em-penca',
+        valor: 15.00,
+        imagem: 'images/Plantas/CatalogoDePlantas/Medias/m-dinheiro-em-penca.jpg',
+        tamanho: 'Média'
+    },
+    // Cróton Brasileirinho
+    {
+        nome: 'Cróton Brasileirinho',
+        valor: 20.00,
+        imagem: 'images/Plantas/CatalogoDePlantas/Medias/m-croton.jpg', // Placeholder: Imagem específica não encontrada
+        tamanho: 'Média'
+    },
+    {
+        nome: 'Cróton Brasileirinho',
+        valor: 40.00,
+        imagem: 'images/Plantas/CatalogoDePlantas/Grandes/g-croton-brasileirinho.jpg',
+        tamanho: 'Grande'
+    },
+    // Lírio do Amazonas
+    {
+        nome: 'Lírio do Amazonas',
+        valor: 35.00,
+        imagem: 'images/Plantas/CatalogoDePlantas/Grandes/g-lirio-amazonas.jpeg',
+        tamanho: 'Grande'
+    },
+    // Lírio da Paz
+    {
+        nome: 'Lírio da Paz',
+        valor: 35.00,
+        imagem: 'images/Plantas/CatalogoDePlantas/Pequenas/p-lirio-da-paz.jpeg',
+        tamanho: 'Pequena'
+    },
+    {
+        nome: 'Lírio da Paz',
+        valor: 35.00,
+        imagem: 'images/Plantas/CatalogoDePlantas/Medias/m-lirio-da-paz.jpeg',
+        tamanho: 'Média'
+    },
+    {
+        nome: 'Lírio da Paz',
+        valor: 35.00,
+        imagem: 'images/Plantas/CatalogoDePlantas/Grandes/g-lirio-da-paz.jpeg',
+        tamanho: 'Grande'
+    },
+    // Palmeira Ráfia (Variados)
+    {
+        nome: 'Palmeira Ráfia (Variados)',
+        valor: 30.00,
+        imagem: 'images/Plantas/CatalogoDePlantas/Medias/m-palmeira-rafia-misto.jpg',
+        tamanho: 'Média'
+    },
+    // Maranta-cinza
+    {
+        nome: 'Maranta-cinza',
+        valor: 30.00,
+        imagem: 'images/Plantas/CatalogoDePlantas/Pequenas/p-maranta-cinza.jpg',
+        tamanho: 'Pequena'
+    },
+    // Zamioculca
+    {
+        nome: 'Zamioculca',
+        valor: 15.00,
+        imagem: 'images/Plantas/CatalogoDePlantas/Pequenas/p-zamiocuca.jpeg',
+        tamanho: 'Pequena'
+    },
+    {
+        nome: 'Zamioculca',
+        valor: 25.00,
+        imagem: 'images/Plantas/CatalogoDePlantas/Medias/m-zamiocuca.jpeg',
+        tamanho: 'Média'
+    },
+    {
+        nome: 'Zamioculca',
+        valor: 50.00,
+        imagem: 'images/Plantas/CatalogoDePlantas/Grandes/g-zamiocuca.jpeg',
+        tamanho: 'Grande'
+    },
+    // Aglaonema Café de Salão
+    {
+        nome: 'Aglaonema Café de Salão',
+        valor: 40.00,
+        imagem: 'images/Plantas/CatalogoDePlantas/Grandes/g-cafe-de-salao.jpeg',
+        tamanho: 'Grande'
+    },
+    // Areca Bambu
+    {
+        nome: 'Areca Bambu',
+        valor: 25.00,
+        imagem: 'images/Plantas/CatalogoDePlantas/Medias/m-areca-bambu.jpg',
+        tamanho: 'Média'
+    },
+    // Grama-Pelo-de-Urso
+    {
+        nome: 'Grama-Pelo-de-Urso',
+        valor: 35.00,
+        imagem: 'images/Plantas/CatalogoDePlantas/Medias/m-grama-pelo-de-urso.jpg',
+        tamanho: 'Média'
+    },
+    // Cheflera
+    {
+        nome: 'Cheflera',
+        valor: 30.00,
+        imagem: 'images/Plantas/CatalogoDePlantas/Medias/m-cheflera.jpeg',
+        tamanho: 'Média'
+    },
+    // Tapete de Rainha
+    {
+        nome: 'Tapete de Rainha',
+        valor: 10.00,
+        imagem: 'images/Plantas/CatalogoDePlantas/Pequenas/p-tapete-de-rainha.jpeg',
+        tamanho: 'Pequena'
+    },
+    // Mudas de Dama da Noite
+    {
+        nome: 'Mudas de Dama da Noite',
+        valor: 10.00,
+        imagem: 'images/Plantas/CatalogoDePlantas/Pequenas/p-mudas-de-dama-da-noite.jpeg',
+        tamanho: 'Pequena'
     }
 ];
 
